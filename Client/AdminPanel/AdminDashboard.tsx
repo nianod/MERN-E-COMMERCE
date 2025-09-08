f073298d0051a47b757d52e15859c2f4b585c74d
nianod <arnoldkk422@gmail.com>
2025-09-08 23:02:26 +0300

    admjin panel

--- Client/AdminPanel/AdminDashboard.tsx
+++ Client/AdminPanel/AdminDashboard.tsx
@@ -1,7 +1,31 @@
- 
+import { FaHome, FaUser, FaInbox, FaCartPlus, FaHandHolding, FaCaretLeft } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div className="bg-black">
+        <div className=" ">
+            <h1 className="text-white font-bold text-2xl">Admin Dashboard</h1>
+        </div>
+        <div>
+            <span>Menu</span>
+            {[
+                { to: '/main/panel' , label: "DashBoard" },
+                { to: '/users' , label: "Users" },
+                { to: '/Products' , label: "Products" },
+                { to: '/orders' , label: "Orders" },
+                { to: '/messages' , label: "Messages" },
+                { to: '/categories' , label: "Categories" },
+            ].map((menu: any, index: any) => (
+              <div key={index}>
+                <Link to={menu.to} >
+                  {menu.label}
+                </Link>
+              </div>
+            ))
+            }
+        </div>
+    </div>
   )
 }
 
