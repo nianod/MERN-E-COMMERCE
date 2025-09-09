4af959f9fa251de7f50f0e19666352b0930fe993
nianod <arnoldkk422@gmail.com>
2025-09-09 18:04:09 +0300

    started backend

--- Server/index.js
+++ Server/index.js
@@ -13,4 +13,6 @@ mongoose.connect(MONGO).then(() => {
     app.listen(PORT, () => {
         console.log(`App running on port ${PORT}`)
     })
+}).catch((error) => {
+    console.log('Error occured ', error)
 })
