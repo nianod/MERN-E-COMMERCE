13e019a855630cc73b4f78b232f90bec3912b04b
nianod <arnoldkk422@gmail.com>
2025-09-17 23:36:57 +0300

    api

--- Server/Routes/productRoute.js
+++ Server/Routes/productRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Product = require('./models.Product.models.js')
-const getProduct = 
+const {getProduct} = require('./Routes.ProductRoute.js')
 
+router.get('/', getProduct)
 router.get('/')
