c8827003089f339175dc071ddd0e57002859f452
nianod <arnoldkk422@gmail.com>
2025-09-17 21:14:51 +0300

    api

--- Client/AdminPanel/Pages/ManageAccount.tsx
+++ Client/AdminPanel/Pages/ManageAccount.tsx
@@ -9,7 +9,7 @@ const ManageAccount = () => {
         <p>Are you sure you want to log out?</p>
         <div className="flex justify-between">
           <button className="bg-red-500 cursor-pointer rounded-lg p-2 text-white font-semibold px-3">Cancel</button>
-          <button className={`bg-red-500 p-2 text-white font-semibold rounded-lg px-3 hover:bg-red-700 ${loading ? "cursor-not-allowed opacity-50" : "cursor-pointer"}`}>Confirm</button>
+          <button className={`bg-red-500 p-2 text-white font-semibold rounded-lg px-3 hover:bg-red-700 cursor-pointer ${loading ? "cursor-not-allowed opacity-50" : "cursor-pointer"}`}>Confirm</button>
         </div>
       </div>
     </div>
