0858b9baa488c63b435f1e0315f11f9fd6d7ef4c
nianod <arnoldkk422@gmail.com>
2025-09-25 12:38:44 +0300

    Axios and CRUD communicating

--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -19,19 +19,30 @@ const Home = () => {
     fetchProducts()
   }, []);
 
-  return (
-    <div className="flex items-center justify-center flex-col overflow-x-hidden mt-20">
-    <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-9 px-3 py-5"> 
+ return (
+    <div className="flex items-center justify-center flex-col overflow-x-hidden mt-16">
+    <div className="grid md:grid-cols-6 sm:grid-cols-1 gap-9 px-3 py-5"> 
       {products.map((product) => (
-        <div key={product._id}>
-          <img src={product.image}/>
-          <b>{product.name}</b>
-          <span>{product.price}</span>
+        <div 
+          key={product._id}
+          className=""
+        >
+          <img
+            src={product.image}
+            alt={product.name}
+            className="h-50 rounded w-50 object-cover"
+           />
+          <p className="text-lg text-white font-semibold capitalize ">{product.name}</p>
+          <span className="text-lg text-amber-300 font-bold mb-1.5">$ {product.price}</span> <br />
+          <button
+            className="bg-fuchsia-500 rounded p-2 px-2 w-full text-white font-bold cursor-pointer hover:bg-fuchsia-600 transition-colors duration-500"
+          >
+            Purchase</button>
         </div>
       ))}
     </div>
     </div>
-  );
+  )
 };
 
 export default Home;
