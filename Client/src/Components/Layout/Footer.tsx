c2fe3f6ce6e0c3c6e21083b1664ee8c317f9e521
nianod <arnoldkk422@gmail.com>
2025-10-02 18:44:55 +0300

    furnished the dashboard

--- Client/src/Components/Layout/Footer.tsx
+++ Client/src/Components/Layout/Footer.tsx
@@ -1,5 +1,17 @@
- import { Link } from "react-router-dom"
+import { FaInstagram, FaFacebook, FaLinkedin, FaTiktok, FaTwitter, FaYoutube } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
 const Footer = () => {
+  
+  const socialLinks = [
+    {label: "instagram", icon: <FaInstagram />, URL : "#"},
+    {label: "Facebook", icon: <FaFacebook />, URL : "#"},
+    {label: "linkedin", icon: <FaLinkedin />, URL : "#"},
+    {label: "tiktok", icon: <FaTiktok />, URL : "#"},
+    {label: "twitter", icon: <FaTwitter />, URL : "#"},
+    {label: "youtube", icon: <FaYoutube />, URL : "#"},
+  ]
+
   return (
     <div className='bottom-0   bg-gray-600 z-40 w-full'>
       <div className="flex justify-center text-white">
@@ -11,6 +23,8 @@ const Footer = () => {
           <Link to="/" className="text-sm hover:underline">Black Friday</Link>
           <Link to="/" className="text-sm hover:underline">Report a product</Link>
           <Link to="/" className="text-sm hover:underline">Product Policies</Link>
+          <Link to="/" className="text-sm hover:underline">Help center</Link>
+          <Link to="/" className="text-sm hover:underline">Chat with us</Link>         
         </div>
         <div className="flex flex-col p-6">
           <h1 className="font-semibold mb-0.5">Make money with us</h1>
@@ -23,10 +37,16 @@ const Footer = () => {
         <div className="flex flex-col p-6">
           <h1 className="font-semibold mb-0.5">About us</h1>
           <Link to="/" className="text-sm hover:underline">Privacy policy</Link>
-          <Link to="/" className="text-sm hover:underline"></Link>
-          <Link to="/" className="text-sm hover:underline">Become an affiliate</Link>
-          <Link to="/" className="text-sm hover:underline">Own shares</Link>
-          <Link to="/" className="text-sm hover:underline">Real estate</Link>
+          <Link to="/" className="text-sm hover:underline">Terms & conditions</Link>
+          <Link to="/" className="text-sm hover:underline">Cookies policy</Link>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>JOIN US ON: </p>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
