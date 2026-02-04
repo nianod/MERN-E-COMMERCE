443e97d18233e41a9e066fc063855d2ccf2c4557
nianod <arnoldkk422@gmail.com>
2026-02-04 22:45:43 +0300

    tested with postman but it did not work..ni mbaya

--- Server/Controllers/authcontroller.js
+++ Server/Controllers/authcontroller.js
@@ -5,27 +5,7 @@ import jwt from  'jsonwebtoken'
 const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString()
 }
-// export const requestLogin = async (req, res) => {
-//   try {
-//     const {email} = req.body
 
-//     if(!email) {
-//       return res.status(400).json({message: 'email is mandatory'})
-//     }
-
-//     const existingUser = await User.findOne({email})
-
-//     return res.status(200).json({
-//       success: true,
-//       message: 'if your email exists in our database, we will send an OTP to your email'
-//     })
-
-//   } catch (error) {
-//     console.error("Error in CheckEmail:", error);
-//     res.status(500).json({ message: "Server error" });  
-//   }
-// }
- 
 
 export const verifyOTP = async (req, res) => {
   const {email, otp} = req.body
