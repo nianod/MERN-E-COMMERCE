61061c9ba42d258c0784583074a281db683ff813
nianod <arnoldkk422@gmail.com>
2026-02-12 17:10:54 +0300

    user Api

--- Server/Controllers/profile.js
+++ Server/Controllers/profile.js
@@ -1,10 +1,22 @@
 import User from "../Models/User";
 
-const findUser = async (request, response) => {
+export const findUser = async (request, response) => {
     try{
         const user = await User.find({})
         response.status(200).json(user)
-    } catch(error){
-        response.status(500).json(error.message)
+    } catch(err){
+        response.status(500).json({message: err.message})
     }
 }
+
+export const findSpecificUser = async (request, response) => {
+    try{
+        const { id } = response.params
+        const user = await User.findById(id)
+        response.status(200).json(user)
+    } catch(err) {
+        response.status(500).json({message: err.message})
+    }
+}
+
+ 
