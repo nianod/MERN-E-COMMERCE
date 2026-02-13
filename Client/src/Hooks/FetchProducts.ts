65ec0bb6e3d102ee3db9b5042b521a65053ef194
nianod <arnoldkk422@gmail.com>
2026-02-13 14:43:25 +0300

    production code

--- Client/src/Hooks/FetchProducts.ts
+++ Client/src/Hooks/FetchProducts.ts
@@ -1,23 +1,23 @@
-import axios from "axios";
-import type { Product } from "../Types/Product"
-import { useEffect, useState } from "react"
+// import axios from "axios";
+// import type { Product } from "../Types/Product"
+// import { useEffect, useState } from "react"
 
-  const [products, setProducts] = useState<Product[]>([]);
+//   const  setProducts] = useState<Product[]>([]);
 
-useEffect(() => {
-    const fetchProducts = async () => {
-    try {
-      const response = await axios.get<Product[]>("http://localhost:8000/api/products");
-      setProducts(response.data);
-      console.log(response.data)
-    } catch (error) {
-      console.error("error occurred", error);
-    }
-  };
-  fetchProducts()
-}, [])
+// useEffect(() => {
+//     const fetchProducts = async () => {
+//     try {
+//       const response = await axios.get<Product[]>("http://localhost:8000/api/products");
+//       setProducts(response.data);
+//       console.log(response.data)
+//     } catch (error) {
+//       console.error("error occurred", error);
+//     }
+//   };
+//   fetchProducts()
+// }, [])
 
    
  
 
-  export default fetchProducts
+//   export default fetchProducts
